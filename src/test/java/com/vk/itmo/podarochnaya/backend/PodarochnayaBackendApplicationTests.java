9e2a44343f8ecec65de916ea55bd196674ea1af7
boris <boris@tomosystech.com>
2024-11-05 16:27:15 +0300

    Add Project files

---
+++ src/test/java/com/vk/itmo/podarochnaya/backend/PodarochnayaBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vk.itmo.podarochnaya.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodarochnayaBackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
