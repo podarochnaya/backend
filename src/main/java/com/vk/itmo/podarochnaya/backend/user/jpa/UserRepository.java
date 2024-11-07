9fb6da5044e98607ff85c520832e6f01cf7196c1
Алексей А. Ларин <aachuyko@yoomoney.ru>
2024-11-07 19:29:52 +0300

    Santa

--- src/main/java/com/vk/itmo/podarochnaya/backend/user/jpa/UserRepository.java
+++ src/main/java/com/vk/itmo/podarochnaya/backend/user/jpa/UserRepository.java
@@ -1,13 +1,13 @@
 package com.vk.itmo.podarochnaya.backend.user.jpa;
 
-import java.util.List;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
